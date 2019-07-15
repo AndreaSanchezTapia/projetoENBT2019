86ba5298fc07b77e1e2c4ba862a52bd095c2eef1
Andrea Sánchez Tapia <katori@gmail.com>
2019-07-15 16:34:43 -0300

    Segundo commit adicionando o script e modificando o readme.

---
+++ R/script.R
@@ -0,0 +1,12 @@
+# Para listar os objetos no workspace
+ls()
+
+#Para remover
+rm(list = ls())
+
+# Qual e a pasta de trabalho
+getwd()
+
+
+a <- c(5, 4, 3, 2, 1)
+a
---
+++ README.txt
@@ -0,0 +1,3 @@
+# Este é o repositório da disciplina ENBT 2019 ministrada por Andrea Sánchez-Tapia, Sara Mortara, Diogo Rocha
+
+Esta aula é sensacional
